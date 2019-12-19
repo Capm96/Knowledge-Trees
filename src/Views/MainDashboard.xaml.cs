697ee66dba8da942ed791ab13d551766527f85d2
Carlos Pereira Motta <carlos.pereiramotta@towerswatson.com>
2019-12-19 10:29:27 +0000

    add shell UI to begin implementing buttons functionality.

--- src/Views/MainDashboard.xaml.cs
+++ src/Views/MainDashboard.xaml.cs
@@ -10,17 +10,16 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace Views
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for MainDashboard.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainDashboard : Window
     {
-        public MainWindow()
+        public MainDashboard()
         {
             InitializeComponent();
         }
